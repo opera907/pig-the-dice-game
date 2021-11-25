751da5ab858fc300e36438fbf6f499fc8c97b67f
dahee <opera9070@gmail.com>
2021-11-25 20:42:33 +0900

    feat: basic setup
    Import btn, total

---
+++ addingtotal.js
@@ -0,0 +1,7 @@
+//setup
+const btnDice = document.querySelector('.btn-dice')
+const btnStop = document.querySelector('.btn-stop')
+const totalP1 = document.querySelector('.total-player1')
+const totalP2 = document.querySelector('.total-player2')
+
+
