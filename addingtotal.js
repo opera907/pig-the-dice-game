2af2a608f6a3074b6ce5be9298799e90b5dc3fb8
dahee <opera9070@gmail.com>
2021-11-25 21:27:58 +0900

    fix : Delect adding.js and move to main.js

--- addingtotal.js
+++
@@ -1,7 +0,0 @@
-//setup
-const btnDice = document.querySelector('.btn-dice')
-const btnStop = document.querySelector('.btn-stop')
-const totalP1 = document.querySelector('.total-player1')
-const totalP2 = document.querySelector('.total-player2')
-
-
